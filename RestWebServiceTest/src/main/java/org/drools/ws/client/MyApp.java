c8872595839f8bd138a2eaf16836e328caae9ca7
Salaboy <salaboy@gmail.com>
2016-02-08 17:57:21 +0000

    - updating services

---
+++ RestWebServiceTest/src/main/java/org/drools/ws/client/MyApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.drools.ws.client;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author salaboy
+ */
+@ApplicationPath("/")
+public class MyApp extends Application{
+
+    public MyApp() {
+    }
+ 
+    
+}
