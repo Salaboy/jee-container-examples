3c0cdfc0e346abb3dd82396279f71afb32ea2c25
Salaboy <salaboy@gmail.com>
2016-02-08 11:03:45 +0000

    - testing wsdeploy.xml

--- drools-was-container-ant/src/main/java/org/drools/was/container/ant/ContainerMain.java
+++ drools-was-container-ant/src/main/java/org/drools/was/container/ant/ContainerMain.java
@@ -1,5 +1,7 @@
 package org.drools.was.container.ant;
 
+import com.ibm.ws.webservices.tools.ant.WSDeploy;
+
 
 
 /**
@@ -8,7 +10,7 @@ package org.drools.was.container.ant;
  */
 public class ContainerMain {
     public static void main(String[] args) {
-        
+       
     }
             
 }
